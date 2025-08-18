bd1dc9bf55fa39f0447b20d2ac49e800b36c2eef
Densanity <denniszixiang@gmail.com>
2025-08-18 20:00:21 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,14 @@
 
 <template>
   <div>
-    <h1>Hello World!</h1>
+    <h1 class="p-8">Hello World!</h1>
   </div>
 </template>
 
 <style scoped>
-
+h1 {
+  background-color: lightgreen;
+  color: blue;
+  font-size: 2em;
+}
 </style>
