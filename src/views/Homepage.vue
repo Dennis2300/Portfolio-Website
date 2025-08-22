cd6f4f95aebcded592ec80ef521abcc6724fdae6
Densanity <denniszixiang@gmail.com>
2025-08-22 19:54:53 +0200

    starting over with homepage gallery

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -25,75 +25,13 @@
       </div>
     </div>
 
-    <div class="gallery-section">
-      <div class="countries-container">
-        <h2
-          v-for="country in countries"
-          class="country"
-          :class="{ 'country--active': selectedCountry === country.id }"
-          @click="selectCountry(country)"
-          :key="country.id"
-        >
-          {{ country.name }}
-        </h2>
-      </div>
-      <div class="images-container">
-        <img
-          v-for="image in homepageImages"
-          class="image"
-          loading="lazy"
-          :src="image.url"
-          :alt="image.alt"
-        />
-      </div>
-    </div>
+    <div class="gallery-section"></div>
   </div>
 </template>
 
 <script setup>
 import { onMounted, ref, computed } from "vue";
 import { supabase } from "../supabaseClient";
-
-const countries = ref([]);
-const allImages = ref([]); // Store all images
-const selectedCountry = ref(1);
-
-// Computed property to filter images
-const homepageImages = computed(() => {
-  return allImages.value.filter(
-    (image) => image.country_id === selectedCountry.value
-  );
-});
-
-function selectCountry(country) {
-  selectedCountry.value = country.id;
-}
-
-async function fetchCountries() {
-  try {
-    const { data, error } = await supabase.from("countries").select("*");
-    if (error) throw error;
-    countries.value = data;
-  } catch (error) {
-    console.error("Error fetching countries:", error);
-  }
-}
-
-async function fetchAllImages() {
-  try {
-    const { data, error } = await supabase.from("homepage_images").select("*");
-    if (error) throw error;
-    allImages.value = data;
-  } catch (error) {
-    console.error("Error fetching homepage images:", error);
-    allImages.value = [];
-  }
-}
-
-onMounted(() => {
-  fetchCountries();
-  fetchAllImages();
-});
 </script>
 
 <style scoped>
@@ -141,67 +79,7 @@ onMounted(() => {
   width: 50%;
 }
 
-.country {
-  cursor: pointer;
-  position: relative;
-  display: inline-block;
-  color: white;
-}
-
-.country::after {
-  content: "";
-  position: absolute;
-  width: 100%;
-  height: 2px;
-  bottom: -3px;
-  left: 0;
-  background-color: currentColor;
-  transform: scaleX(0);
-  transform-origin: bottom right;
-  transition: transform 0.3s ease;
-}
-
-.country:hover::after {
-  transform: scaleX(1);
-  transform-origin: bottom left;
-}
-
-.country--active::after {
-  transform: scaleX(1);
-  transform-origin: bottom left;
-}
-
 .gallery-section {
   min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.countries-container {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 66px;
-  font-size: 2em;
-  letter-spacing: 1px;
-  font-family: "Montserrat", sans-serif;
-}
-
-.images-container {
-  margin-top: 66px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 66px;
-}
-
-.image {
-  width: 400px;
-  height: 600px;
-  object-fit: cover;
-  border-radius: 15px;
-  box-shadow: 0 8px 24px rgba(0, 0, 0, 0.8);
 }
 </style>
