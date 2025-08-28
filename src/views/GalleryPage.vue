40293dc65a2d0dafd78730daa33a2f947b7c426c
Densanity <denniszixiang@gmail.com>
2025-08-28 18:57:28 +0200

    Add dynamic country sections to GalleryPage
    GalleryPage now fetches and displays a list of countries from Supabase, rendering each as a section. Added basic layout and styling for the gallery container and sections.

--- src/views/GalleryPage.vue
+++ src/views/GalleryPage.vue
@@ -1,7 +1,59 @@
 <template>
-  <h1>Gallery</h1>
+  <div class="gallery-container">
+    <h1 class="text-center">Gallery</h1>
+    <div class="gallery-sections-container">
+      <div class="gallery-section" v-for="country in countries">
+        <h2>{{ country.name }}</h2>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted, ref } from "vue";
+import { supabase } from "../supabaseClient";
 
-<style src=""></style>
+const countries = ref([]);
+
+async function fetchCountries() {
+  const { data, error } = await supabase.from("countries").select("*");
+
+  countries.value = data;
+
+  if (error) {
+    console.error("Error fetching countries:", error);
+  } else {
+    console.log("Countries:", data);
+  }
+}
+
+onMounted(() => {
+  fetchCountries();
+});
+</script>
+
+<style scoped>
+.gallery-container {
+  min-height: 100vh;
+}
+
+.gallery-container h1 {
+  font-family: "Alfa Slab One", sans-serif;
+  font-size: 4rem;
+  margin: 20px 0;
+}
+
+.gallery-sections-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.gallery-section {
+  height: 66vh;
+  width: 90vw;
+  background-color: darkslateblue;
+  margin-bottom: 50px;
+}
+</style>
