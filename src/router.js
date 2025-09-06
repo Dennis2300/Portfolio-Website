9f265d685b166b55ba2dda17bcbbaa509e741244
Densanity <denniszixiang@gmail.com>
2025-09-06 16:55:34 +0200

    Add new country gallery routes
    Added routes for China, Greece, Hungary, Netherlands, and Sweden gallery pages. Updated Denmark and Germany routes to be under /gallery. Imported corresponding view components.

--- src/router.js
+++ src/router.js
@@ -1,14 +1,24 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Homepage from "./views/Homepage.vue";
+import GalleryPage from "./views/GalleryPage.vue";
+import ChinaPage from "./views/ChinaPage.vue";
 import DenmarkPage from "./views/DenmarkPage.vue";
 import GermanyPage from "./views/GermanyPage.vue";
-import GalleryPage from "./views/GalleryPage.vue";
+import GreecePage from "./views/GreecePage.vue";
+import HungaryPage from "./views/HungaryPage.vue";
+import NetherlansPage from "./views/NetherlandsPage.vue";
+import SwedenPage from "./views/SwedenPage.vue";
 
 const routes = [
   { path: "/", component: Homepage },
   { path: "/gallery", component: GalleryPage },
-  { path: "/denmark", component: DenmarkPage },
-  { path: "/germany", component: GermanyPage },
+  { path: "/gallery/denmark", component: DenmarkPage },
+  { path: "/gallery/germany", component: GermanyPage },
+  { path: "/gallery/china", component: ChinaPage },
+  { path: "/gallery/greece", component: GreecePage },
+  { path: "/gallery/hungary", component: HungaryPage },
+  { path: "/gallery/netherlands", component: NetherlansPage },
+  { path: "/gallery/sweden", component: SwedenPage },
 ];
 
 const router = createRouter({
